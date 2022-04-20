116272f4e66cd106a4ed42fe93029e95a3795b80
Angela <98893476+amyers0217@users.noreply.github.com>
2022-04-20 12:23:15 -0400

    Add files via upload

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace AngelaMyersChatApp.Client
+namespace AngelaMyersChat.Client
 {
     public class Program
     {
